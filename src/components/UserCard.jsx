8c40cdfdfdf6396332c0b65d1583bacfdc06887b
ShilpeshThakur <thakur.shilpesh007@gmail.com>
2025-08-05 01:18:30 +0530

    card edit

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -7,13 +7,13 @@ const UserCard = ({user}) => {
     <div className="card bg-base-300 w-96 shadow-sm">
         <figure>
             <img
-            src={user.photoUrl}
+            src={photoUrl}
             alt="Photo" />
         </figure>
         <div className="card-body">
             <h2 className="card-title">{firstName + " " +lastName}</h2>
             {age && gender && <p>{age + ", " + gender }</p>}
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <p>{about}</p>
             <div className="card-actions justify-center m-4">
                 <button className="btn btn-primary">Ignore</button>
                 <button className="btn btn-secondary">Interested</button>
