7a2f98922169a22707a8b52375b32b2797675017
ShilpeshThakur <thakur.shilpesh007@gmail.com>
2025-08-04 18:11:59 +0530

    created component

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer bg-base-300 text-neutral-content items-center p-4 fixed bottom-0">Footer</div>
+  )
+}
+
+export default Footer
